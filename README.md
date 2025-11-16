d395b52d40569ffb4a2d980a5838c3bad4becf4e
carina <carina@sparkles.io>
2025-11-16 10:57:26 +0100

    project finished

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-section-23
 authentication
+
+In the devTools / Application tab / Local Storage there is a list of items stored in localStorage
--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,15 +1,34 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import {Outlet, useLoaderData, useSubmit} from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import {useEffect} from "react";
+import {getTokenDuration} from "../util/auth";
 
 function RootLayout() {
-  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+    useEffect(() => {
+        if (!token) return;
+
+        if (token === 'EXPIRED') {
+            submit(null, {actions: '/logout', method: 'post'});
+            return;
+        }
+
+        const tokenDuration = getTokenDuration();
+        console.log('token duration ', tokenDuration);
+
+        setTimeout(() => {
+            submit(null, {action: '/logout', method: 'post'})
+        }, tokenDuration)
+    }, [token, submit]);
 
   return (
     <>
       <MainNavigation />
       <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+
         <Outlet />
       </main>
     </>
